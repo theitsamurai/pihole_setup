58b9604ca5cce27ef24232e87a95ff8912069e17
theitsamurai <63486488+theitsamurai@users.noreply.github.com>
2020-09-16 23:50:36 -0400

    Update raspios_lite.sh

--- raspios_lite.sh
+++ raspios_lite.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# update apt
+sudo apt update -y && upgrade -y && autoremove -y
+
 # remove chromium
 sudo apt purge -y chromium && autoremove
+
+# install iceweasel (aka Firefox)
+sudo apt install -y iceweasel
